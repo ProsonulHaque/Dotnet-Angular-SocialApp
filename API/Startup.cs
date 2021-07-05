5b9062f02aae9b7b9169b9a36ad4cd1a59e37f32
Prosonul Haque <proshonulhaq@gmail.com>
2021-07-05 18:59:34 +0600

    Added register and login endpoints, used password hash and password salt for safe storage of passwords, added JWT bearer authentication token to secure endpoints and extended IServiceCollection class to keep ConfigureServices method clean (EOS 4)

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using API.Extensions;
 
 namespace API
 {
@@ -24,17 +16,17 @@ namespace API
             _config = config;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices(_config);
+            
+            services.AddIdentityServices(_config);
+
             services.AddCors();
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+            
             services.AddControllers();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -58,6 +50,8 @@ namespace API
             app.UseCors(policy => policy.AllowAnyHeader()
                                         .AllowAnyMethod()
                                         .WithOrigins("https://localhost:4200"));
+
+            app.UseAuthentication();
             
             app.UseAuthorization();
 
